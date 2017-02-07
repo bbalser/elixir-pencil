74056dd33f15c267198bc1b70f374258aac957c3
Brian Balser <brian.balser@gmail.com>
2017-02-07 12:04:00 -0500

    Pencil now has durability

--- lib/pencil.ex
+++ lib/pencil.ex
@@ -1,7 +1,25 @@
 defmodule Pencil do
 
-  def write(paper, string) do
-    Paper.write(paper, string)
+  def new(durability) do
+    {:ok, pid} = Agent.start_link(fn -> durability end)
+    pid
+  end
+
+  def write(pencil, paper, string) do
+    durability = get_and_update_durability(pencil, String.length(string))
+    output = slice_string(string, durability)
+          |> String.pad_trailing(String.length(string))
+
+    Paper.write(paper, output)
+  end
+
+  defp get_and_update_durability(pencil, amount_to_use) do
+    Agent.get_and_update(pencil, fn durability -> {durability, max(0, durability - amount_to_use)} end)
+  end
+
+  defp slice_string(_string, length) when length <= 0, do: ""
+  defp slice_string(string, length) do
+    String.slice(string, 0..(length-1))
   end
 
 end
--- test/pencil_test.exs
+++ test/pencil_test.exs
@@ -1,12 +1,35 @@
 defmodule PencilTest do
   use ExUnit.Case, aync: true
 
-  test "pencil will write text to sheet of paper" do
+  setup do
     sheet = Paper.new_sheet
+    {:ok, [sheet: sheet]}
+  end
 
-    Pencil.write(sheet, "It was the best of times, it was the worst of times")
+  test "pencil will write text to sheet of paper", ctx do
+    pencil = Pencil.new(100)
 
-    assert "It was the best of times, it was the worst of times" == Paper.read(sheet)
+    Pencil.write(pencil, ctx[:sheet], "It was the best of times, it was the worst of times")
+
+    assert "It was the best of times, it was the worst of times" == Paper.read(ctx[:sheet])
+  end
+
+  test "when a pencil goes dull, it writes spaces to paper instead of text", ctx do
+    pencil = Pencil.new(5)
+
+    Pencil.write(pencil, ctx[:sheet], "possibilities")
+
+    assert "possi        " == Paper.read(ctx[:sheet])
+  end
+
+  test "the pencil tracks its durability across multiple writes", ctx do
+    pencil = Pencil.new(10)
+
+    Pencil.write(pencil, ctx[:sheet], "Howdy ")
+    Pencil.write(pencil, ctx[:sheet], "Doody ")
+    Pencil.write(pencil, ctx[:sheet], "loves to ride his horse")
+
+    assert "Howdy Dood                         " == Paper.read(ctx[:sheet])
   end
 
 end
